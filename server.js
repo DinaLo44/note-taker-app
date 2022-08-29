f224bf4953744ee61bb8861d12fc2d6ed33950b8
DinaLo <dinalo.brady@gmail.com>
2022-08-28 22:40:33 -0600

    set up get, post and still working n delete method

---
+++ server.js
@@ -0,0 +1,68 @@
+//Requiring dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const notes = require('./db/db.json');
+const uuid = require('uuid');
+
+//Setting up the app and ports
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Middleware for parsing the application
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Seeting up the static middleware
+app.use(express.static("public"));
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+//Setting up the API route/ GET method
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'))
+}
+);
+
+//API route / POST method
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid.v4()
+    console.log(newNote)
+    notes.push(newNote)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.sendFile(path.join(__dirname, './db/db.json'))
+});
+
+
+// DELETE Method
+app.delete("/api/notes/:id", (req, res) => {
+const deleteId = parseInt(req.params.id);
+for(let i = 0; i < notes.length; i++) {
+    if (notes[i].id === deleteId) {
+    notes.splice(i, 1);
+    }
+}
+console.log(notes);
+
+fs.writeFileSync('./db/db.json', JSON.stringify(notes), error => {
+    if (error) throw error;
+    res.json(notes);
+});
+    
+});
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+
+app.listen(PORT, () =>
+    console.log(`Serving Note Taker app on port ${PORT}`));
